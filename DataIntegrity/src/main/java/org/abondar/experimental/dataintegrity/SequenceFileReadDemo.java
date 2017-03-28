638513e9918cbb7d4f63cc013fdd62d453463f26
abondar24 <abondar1992@gmail.com>
2017-03-28 13:52:25 -0700

    Compression , Decompression, SequenceFiles

---
+++ DataIntegrity/src/main/java/org/abondar/experimental/dataintegrity/SequenceFileReadDemo.java
@@ -0,0 +1,39 @@
+package org.abondar.experimental.dataintegrity;
+
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.util.ReflectionUtils;
+
+import java.io.IOException;
+
+public class SequenceFileReadDemo {
+    public static void main(String[] args) {
+        String uri = args[0];
+        Configuration conf = new Configuration();
+        Path path = new Path(uri);
+
+        SequenceFile.Reader reader = null;
+        SequenceFile.Reader.Option fileOpt = SequenceFile.Reader.file(path);
+
+
+        try {
+            reader = new SequenceFile.Reader(conf,fileOpt);
+            Writable key = (Writable) ReflectionUtils.newInstance(reader.getKeyClass(),conf);
+            Writable value = (Writable) ReflectionUtils.newInstance(reader.getValueClass(),conf);
+            long pos = reader.getPosition();
+
+            while (reader.next(key,value)){
+                String syncSeen = reader.syncSeen() ? "*" : "";
+                System.out.printf("[%s%s]\t%s\t%s\n",pos,syncSeen,key,value);
+            }
+        }catch (IOException e){
+            System.err.println(e.getMessage());
+        } finally {
+            IOUtils.closeStream(reader);
+        }
+    }
+}
---
+++ DataIntegrity/src/main/java/org/abondar/experimental/dataintegrity/TextIterator.java
@@ -0,0 +1,18 @@
+package org.abondar.experimental.dataintegrity;
+
+
+import org.apache.hadoop.io.Text;
+
+import java.nio.ByteBuffer;
+
+public class TextIterator {
+    public static void main(String[] args) {
+        Text t = new Text("\u0041\u00DF\uD801\uDC00");
+        ByteBuffer buffer = ByteBuffer.wrap(t.getBytes(),0,t.getLength());
+        int cp;
+
+        while (buffer.hasRemaining() && (cp = Text.bytesToCodePoint(buffer))!= -1){
+            System.out.println(Integer.toHexString(cp));
+        }
+    }
+}
