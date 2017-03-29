e36726c1252144c70c9f0389bc7d8b957f13c6e6
abondar24 <abondar1992@gmail.com>
2017-03-28 18:37:12 -0700

    MapReduce extended

---
+++ MapReduceApp/src/main/java/org/abondar/experimental/mapreduceapp/RecordParser.java
@@ -0,0 +1,49 @@
+package org.abondar.experimental.mapreduceapp;
+
+
+import org.apache.hadoop.io.Text;
+
+public class RecordParser {
+    private static final int MISSING_TEMP = 9999;
+    private String year;
+    private int airTemp;
+    private String quality;
+    private boolean airTempMalformed;
+
+    public void parse(String record) {
+        year = record.substring(15, 19);
+        String airTempStr;
+
+        airTempMalformed = false;
+        if (record.charAt(87) == '+') {
+            airTempStr = record.substring(88, 92);
+        } else {
+            airTempStr = record.substring(87, 92);
+        }
+
+        airTemp = Integer.parseInt(airTempStr);
+        quality = record.substring(92, 93);
+    }
+
+    public void parse(Text record) {
+        parse(record.toString());
+    }
+
+    public boolean isValidTemperature() {
+        return !airTempMalformed && airTemp != MISSING_TEMP && quality.matches("[01459]");
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public int getAirTemp() {
+        return airTemp;
+    }
+
+    public boolean isAirTempMalformed() {
+        return airTempMalformed;
+    }
+
+
+}
---
+++ MapReduceApp/src/main/java/org/abondar/experimental/mapreduceapp/utils/ConfigurationPrinter.java
@@ -0,0 +1,33 @@
+package org.abondar.experimental.mapreduceapp.utils;
+
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import java.util.Map;
+
+public class ConfigurationPrinter extends Configured implements Tool {
+
+    static {
+        Configuration.addDefaultResource("hadoop-cluster.xml");
+        Configuration.addDefaultResource("hadoop-local.xml");
+        Configuration.addDefaultResource("hadoop-localhost.xml");
+    }
+
+
+    @Override
+    public int run(String[] strings) throws Exception {
+        Configuration conf = getConf();
+        for (Map.Entry<String, String> entry : conf) {
+            System.out.printf("%s=%s\n", entry.getKey(), entry.getValue());
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int exitCode = ToolRunner.run(new ConfigurationPrinter(), new String[]{"hadoop-cluster.xml"});
+        System.exit(exitCode);
+    }
+}
